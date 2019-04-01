e99cd3f49d38eebc589bf3a8de1739ec7cb55c59
huangzisheng <huangzs@aheading.com>
2019-04-01 18:00:15 +0800

    Initial commit

---
+++ app/src/main/java/fly/rotate/com/androidjetpackstudy/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package fly.rotate.com.androidjetpackstudy.ui.main
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+    private val _data = MutableLiveData<String>()
+    val data:LiveData<String>
+    get() = _data
+
+    init {
+        _data.value="Hello,Jetpack!"
+    }
+}
